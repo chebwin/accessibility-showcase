3c399c04301d443664977a23cbf7e6300c353adf
Graham Wedderman <graham.wedderburn@ba.com>
2023-04-21 08:53:23 +0100

    started following the agreed plan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Accessibility Showcase';
+  title = 'Accessibility Showcase - Demonstration';
 }
